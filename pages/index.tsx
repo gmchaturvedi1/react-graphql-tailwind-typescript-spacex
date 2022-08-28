8eecc659a44c8486c4dabc9642202c7b738a67d1
Gajendra Nath Chaturvedi <gajendranathchaturvedi@Gajendras-MacBook-Pro.local>
2022-08-28 12:25:14 +0530

    Add:Launch Profile,graphqlk-codegen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
 import LaunchList from '../src/components/LaunchList';
-import LaunchProfile from './src/components/LaunchProfile';
-
-import './App.css';
+import LaunchProfile from '../src/components/LaunchProfile';
 
 const App = () => {
- const [id, setId] = React.useState(42);
- const handleIdChange = React.useCallback((newId) => {
+ const [id, setId] = React.useState<number>(42);
+ const handleIdChange = React.useCallback((newId: number) => {
   setId(newId);
  }, []);
 
  return (
-  <div className='App'>
+  <div>
    <LaunchList handleIdChange={handleIdChange} />
    <LaunchProfile id={id} />
   </div>
